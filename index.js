d60f38516a06e3cb7f1b10ae6e1a2e19099ccce7
Carlos Arthur <49536258+CarlosArthurM@users.noreply.github.com>
2024-07-18 19:45:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+var txtanimado = document.getElementById('txtanimado');
+var typewriter = new Typewriter(txtanimado, { loop: true });
+typewriter.typeString('PROGRAMADOR FRONT-END')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('DESIGNER')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('EDITOR DE VIDEO')
+    .pauseFor(2500)
+    .deleteAll()
+    .start();
+    
+ const mode = document.getElementById("mode_icon");
+
+mode.addEventListener('click', () => {
+  if(mode.classList.contains('fa-moon')){
+    mode.classList.remove('fa-moon');
+    mode.classList.add('fa-sun');
+    return;
+  }
+
+  mode.classList.add('fa-moon');
+  mode.classList.remove('fa-sun');
+});
+
+document.getElementById('mode_icon').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+});
+
+function abreModal() {
+  const modal = document.getElementById('myModal');
+  modal.showModal();
+}
+
+function fechaModal() {
+  const modal = document.getElementById('myModal');
+  modal.close();
+}
